f911f96efa60d30fa4544b7e9f1efed4c81a31d5
ApolloIV92 <rmorzel@gmail.com>
2022-07-18 23:29:36 -0500

    Add image credits and README

---
+++ README.md
@@ -0,0 +1,14 @@
+This page was created for The Odin Project (https://www.theodinproject.com/). The goal of this project was to practice using webpack
+to bundle modules together (thus the tab style design that calls a js module to build each page dynamically).
+
+Image credits:
+
+- Home page image (https://unsplash.com/photos/dXGl11H1Kbg) by Harsh Pandey (https://unsplash.com/@harsh_clicks__)
+
+- Menu image one (https://unsplash.com/photos/zI27XpN1GdA) by René Porter (https://unsplash.com/@reneporter)
+
+- Menu image two (https://unsplash.com/photos/L82-kkEBOd0) by Manki Kim (https://unsplash.com/@kimdonkey)
+
+- Menu image three (https://unsplash.com/photos/uXS73PNgYTc) by Sharon McCutcheon (https://unsplash.com/@sharonmccutcheon)
+
+- Contact us image (https://unsplash.com/photos/C24MUnUykY0) by Sereina (https://unsplash.com/@seri_lia)
--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@ import './styles.css';
 import { loadHomePage } from "./homePage.js";
 import { loadMenuPage } from "./menuPage.js";
 import { loadContactPage } from "./contactPage.js";
-import Background from './background.jpg'
+import Github from './github-logo.png';
+
 
 function initPage() {
     const homePageBtn = document.getElementById("homePage");
     const menuBtn = document.getElementById("menu");
     const contactBtn = document.getElementById("contact");
+    
+    const footer = document.getElementById('footer');
+    
+    const githubLogo = new Image();
+    githubLogo.src = Github;
+    githubLogo.width = "40";
+
+    const githubLink = document.createElement('a');
+    githubLink.href = 'https://github.com/ApolloIV92/';
+    githubLink.innerHTML = "<img src="+Github+" width=40></img>";
+
+    const footerText = document.createElement('span');
+    footerText.innerHTML = "Created by Rick Orzel for\
+     <a href=https://www.theodinproject.com/>The Odin Project.</a>"
+
+     const creditText = document.createElement('span');
+     creditText.innerHTML = "Background image by\
+     <a href=https://unsplash.com/@stefany_andrade>Stefany Andrade</a>\
+     on <a href=https://unsplash.com/>Unsplash.</a>. "
+
+    footer.appendChild(githubLink);
+    footer.appendChild(footerText);
 
     homePageBtn.addEventListener("click", () => loadHomePage());
     menuBtn.addEventListener("click", () => loadMenuPage());
